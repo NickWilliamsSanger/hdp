dfbf79189260b1396d87e89355e355f715efbd29
Nick Williams <nw14@sanger.ac.uk>
2024-04-18 13:05:26 +0100

    Fix vector if checking

--- R/hdp_setdata.R
+++ R/hdp_setdata.R
@@ -33,7 +33,7 @@ hdp_setdata <- function(hdp, dpindex, data){
     stop("dpindex must be positive integers no greater than
          numdp(hdp) with no duplicates")
   }
-  if (!class(data) %in% c("matrix", "data.frame")) {
+  if (length(intersect(class(data),c("matrix", "data.frame")))==0) {
     stop("data must be data.frame or matrix")
   }
   if (nrow(data)!=length(dpindex)) stop("nrow(data) must equal length(dpindex)")
